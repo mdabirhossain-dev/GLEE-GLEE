84e86e1a15f36195ee6596e760df5c1a81d434a7
Md Abir Hossain <hossain.mdabir@gmail.com>
2024-02-14 11:30:08 +0600

    Initial Commit

---
+++ GLEE GLEE/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  GLEE GLEE
+//
+//  Created by Md Abir Hossain on 14/2/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ GLEE GLEE/GLEE_GLEEApp.swift
@@ -0,0 +1,17 @@
+//
+//  GLEE_GLEEApp.swift
+//  GLEE GLEE
+//
+//  Created by Md Abir Hossain on 14/2/24.
+//
+
+import SwiftUI
+
+@main
+struct GLEE_GLEEApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
